8b701553acc6e83b8700e7be33087fed633e46d8
Ning Yu <ningyu321@gmail.com>
2017-09-23 10:44:43 +0800

    requestmethod

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -26,7 +26,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
