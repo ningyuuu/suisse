9f6ba4ddc09692dc28da89ac3ad9225c9983a88f
Ning Yu <ningyu321@gmail.com>
2017-09-23 10:53:37 +0800

    json1

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -28,8 +28,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import javax.json.JsonObject;
 
+import javax.json.JsonObject;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.ResultSet;
