0ab899e3f71e0074eec13ce3c3b5adbea78da7d0
sy <smily_facy@hotmail.com>
2017-09-23 17:15:21 +0800

    emptyarea Wrapper added

---
+++ src/main/java/com/example/EmptyAreaWrapper.java
@@ -0,0 +1,79 @@
+package com.example;
+
+import java.util.*;
+
+public class EmptyAreaWrapper {
+    private Container container;
+    private Rectangle rectangle;
+    private Square square;
+    private Circle circle;
+    
+    public Container getContainer(){
+    	return container;
+    }
+    public Rectangle getRectangle(){
+    	return rectangle;
+    }
+    public Square getSquare(){
+    	return square;
+    }
+    public Circle getCircle(){
+    	return circle;
+    }
+
+    class Container {
+    	private Coordinate coordinate;
+    	private double width;
+    	private double height;
+
+    	public Coordinate getCoordinate(){
+    		return coordinate;
+    	}
+    	public double getWidth(){
+    		return width;
+    	}
+    	public double getHeight(){
+    		return height;
+    	}
+    }
+
+    class Rectangle {
+    	private Coordinate coordinate;
+    	private double width;
+    	private double height;
+
+    	public Coordinate getCoordinate(){
+    		return coordinate;
+    	}
+    	public double getWidth(){
+    		return width;
+    	}
+    	public double getHeight(){
+    		return height;
+    	}
+    }
+
+    class Circle {
+    	private Coordinate center;
+    	private double radius;
+
+    	public Coordinate getCenter(){
+    		return center;
+    	}
+    	public double getRadius(){
+    		return radius;
+    	}
+    }
+
+    class Container {
+    	private int X;
+    	private int Y;
+
+    	public int return getX(){
+    		return X;
+    	}
+    	public int return getY(){
+    		return Y;
+    	}
+    }
+}
