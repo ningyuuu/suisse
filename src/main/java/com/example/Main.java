5dce586712fe2584a9d494616fa4e627b60bd837
Ning Yu <ningyu321@gmail.com>
2017-09-23 11:02:29 +0800

    requestbody

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -28,6 +28,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
