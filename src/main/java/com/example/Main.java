6a03d6112f8bfbabfbf29598f39beb4bd6f8a0d7
Ning Yu <ningyu321@gmail.com>
2017-09-23 10:42:07 +0800

    requestparamm

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -24,7 +24,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
