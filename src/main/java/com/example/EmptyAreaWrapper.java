81c83ce5860949154f810d711d8a62ae94d15bd3
Ning Yu <ningyu321@gmail.com>
2017-09-24 10:21:18 +0800

    remove emptyarea code

--- src/main/java/com/example/EmptyAreaWrapper.java
+++ src/main/java/com/example/EmptyAreaWrapper.java
@@ -1,63 +1,63 @@
-package com.example;
+// package com.example;
 
-import java.util.*;
+// import java.util.*;
 
-public class EmptyAreaWrapper {
-    private Rectangle container;
-    private Rectangle rectangle;
-    private Square square;
-    private Circle circle;
+// public class EmptyAreaWrapper {
+//     private Rectangle container;
+//     private Rectangle rectangle;
+//     private Square square;
+//     private Circle circle;
     
-    public Container getContainer(){
-    	return container;
-    }
-    public Rectangle getRectangle(){
-    	return rectangle;
-    }
-    public Square getSquare(){
-    	return square;
-    }
-    public Circle getCircle(){
-    	return circle;
-    }
+//     public Container getContainer(){
+//     	return container;
+//     }
+//     public Rectangle getRectangle(){
+//     	return rectangle;
+//     }
+//     public Square getSquare(){
+//     	return square;
+//     }
+//     public Circle getCircle(){
+//     	return circle;
+//     }
 
-    class Rectangle {
-    	private Coordinate coordinate;
-    	private double width;
-    	private double height;
+//     class Rectangle {
+//     	private Coordinate coordinate;
+//     	private double width;
+//     	private double height;
 
-    	public Coordinate getCoordinate(){
-    		return coordinate;
-    	}
-    	public double getWidth(){
-    		return width;
-    	}
-    	public double getHeight(){
-    		return height;
-    	}
-    }
+//     	public Coordinate getCoordinate(){
+//     		return coordinate;
+//     	}
+//     	public double getWidth(){
+//     		return width;
+//     	}
+//     	public double getHeight(){
+//     		return height;
+//     	}
+//     }
 
-    class Circle {
-    	private Coordinate center;
-    	private double radius;
+//     class Circle {
+//     	private Coordinate center;
+//     	private double radius;
 
-    	public Coordinate getCenter(){
-    		return center;
-    	}
-    	public double getRadius(){
-    		return radius;
-    	}
-    }
+//     	public Coordinate getCenter(){
+//     		return center;
+//     	}
+//     	public double getRadius(){
+//     		return radius;
+//     	}
+//     }
 
-    class Container {
-    	private int X;
-    	private int Y;
+//     class Container {
+//     	private int X;
+//     	private int Y;
 
-    	public int return getX(){
-    		return X;
-    	}
-    	public int return getY(){
-    		return Y;
-    	}
-    }
-}
+//     	public int return getX(){
+//     		return X;
+//     	}
+//     	public int return getY(){
+//     		return Y;
+//     	}
+//     }
+// }
